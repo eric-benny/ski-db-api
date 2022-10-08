a22158432844f4939a29a69db3abdab1027e658e
eric-benny <59672346+eric-benny@users.noreply.github.com>
2022-10-07 21:22:41 -0400

    remove ski comps on delete

--- functions/deleteSkiComp.js
+++ functions/deleteSkiComp.js
@@ -1,4 +1,5 @@
 const { SkiCompModel } = require('../database/model/skiComp');
+const { SkiModel } = require('../database/model/ski');
 const response = require('../libs/response');
 const { connectToDatabase } = require('../libs/db');
 
@@ -7,6 +8,13 @@ export const deleteSkiComp = async (event, context) => {
   try {
     await connectToDatabase();
     const skiCompId = event.pathParameters ? event.pathParameters.skiCompId : undefined;
+
+    const comp = await SkiCompModel.findOne({ _id: skiCompId });
+
+    const primarySki = await SkiModel.findOne({ _id: comp.primarySki });
+    await SkiModel.updateOne({_id: primarySki._id}, {skiComps: primarySki.skiComps.filter(c => c.toString() !== comp._id.toString())});
+    const secondarySki = await SkiModel.findOne({ _id: comp.secondarySki });
+    await SkiModel.updateOne({_id: secondarySki._id}, {skiComps: secondarySki.skiComps.filter(c => c.toString() !== comp._id.toString())});
 
     await SkiCompModel.deleteOne({_id: skiCompId});
 
